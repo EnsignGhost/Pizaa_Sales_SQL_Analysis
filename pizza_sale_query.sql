b9b480e71b31204a6bfbcf61b45d560f47a640df
Deepak Kumar <113766298+EnsignGhost@users.noreply.github.com>
2024-10-16 11:15:17 +0530

    pizza_sale_query.sql

--- pizza_sale_query.sql
+++ pizza_sale_query.sql
@@ -147,6 +147,17 @@ pizza_types
 pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
         JOIN
 order_details ON order_details.pizza_id = pizzas.pizza_id
+
+ -- Analyze the cumulative revenue generated over time. 
+
+select order_date , sum(revenue) over(order by order_date) as cum_revenue
+from
+(select orders.order_date , sum(order_details.quantity * pizzas.price ) as revenue
+from order_details join pizzas
+on order_details.pizza_id = pizzas.pizza_id
+join orders
+on orders.order_id = order_details.order_id
+group by orders.order_date) as sales;
 GROUP BY pizza_types.category
 ORDER BY revenue DESC;
 
